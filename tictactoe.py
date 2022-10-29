b2d08745b1b4035ed998511f2b5ae01faccc283e
Nihal Suri <nihal@example.com>
2022-10-29 17:22:23 +0200

    Added PLAYER, ACTIONS, RESULT, WINNER

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
 
 X = "X"
 O = "O"
@@ -22,28 +23,87 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
-
+    x_freq = 0
+    o_freq = 0
+    
+    x_freq = sum([i.count(X) for i in board])
+    o_freq = sum([i.count(O) for i in board])
+    
+    if x_freq > o_freq : return O
+    else : return X
+    
+    
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    
+    for r in range(3):
+        for c in range(3): 
+            if board[r][c] == EMPTY: 
+                possible_actions.add((r , c))
+    
+    return possible_actions 
+        
+    
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action not in actions(board): 
+        raise Exception("Action not valid -- Space already occupied. Please select new space")
+    
+    r , c = action 
+    copy_board = deepcopy(board)
+    copy_board[r][c] = player(board)
+    return copy_board
+    
 
 
+def row_check(board, player):
+    for r in range(3):
+        if board[r][0] == player and board[r][1] == player and board[r][2] == player: 
+            return True 
+    return False 
+
+
+def col_check(board, player): 
+    for c in range(3): 
+        if board[0][c] == player and board[1][c] == player and board[2][c] == player: 
+            return True 
+    return False 
+
+
+def diag_check(board, player): 
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player: 
+        return True 
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player: 
+        return True 
+    else: 
+        return False 
+        
+
+    
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    
+    if player == X: 
+        if row_check(board, player) or col_check(board, player) or diag_check(board, player): 
+            return X
+    elif player == O: 
+        if row_check(board, player) or col_check(board, player) or diag_check(board, player): 
+            return O 
+    else: 
+        return EMPTY
+    
+    
+    
 
 
 def terminal(board):
